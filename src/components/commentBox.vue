8edbed38e5d8e7f64f9870c348c6d09c9b585f29
r0808 <arthurdelophem3@gmail.com>
2022-11-16 14:13:20 +0100

    feat: add base frontend structure

---
+++ src/components/commentBox.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    <div class="panel">
+        <div class="panel__info">
+            <h1 class="panel__username">Elon Tusk</h1>
+            <p class="panel__subtext"><strong>No space on Subway Station</strong></p>
+        </div>
+        <div class="comment__view">
+            <div class="comment__iterate">
+                <p class="comment__username"><strong>wajoo</strong></p>
+                <p class="comment__txt">i am txt</p>
+            </div>
+            <div class="comment__interaction">
+                <div class="comment__input"></div>
+                <div class="comment__button"></div>
+            </div>
+        </div>
+    </div>
+</template>
